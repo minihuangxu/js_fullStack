13ab8d3abd56c9fefb1152a07fe18ecc353a706b
minihuangxu <2682249030@qq.com>
2019-05-05 18:53:16 +0800

    landRover

---
+++ wxapp/landRover/pages/vehicles/index.js
@@ -0,0 +1,87 @@
+// pages/vehicles/index.js
+
+const db = require('../../assets/db.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    entity: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    let id = options.id;
+    id = parseInt(id);
+    const entity = db.vehicles.find((item) => {
+      // return item.id == id;
+      return item.id === id;
+    })
+    console.log(id, entity, db.vehicles);
+    if (entity) {
+      this.setData({
+        entity
+      })
+      wx.setNavigationBarTitle({
+        title: entity.header
+      })
+    } else {
+      wx.redirectTo({
+        url: '../index/index'
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
