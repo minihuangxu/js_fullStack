62da77650deb03b5d421385e501de8e870c7da5c
minihuangxu <2682249030@qq.com>
2019-06-17 10:05:55 +0800

    event

---
+++ node/event/event.js
@@ -0,0 +1,22 @@
+// 原生的一个模块
+// 供其他模块使用 比如 http res 继承自 Event
+const Events = require('events');
+const ev = new Events();
+
+ev.on('open', () => {
+  console.log('open 发生了');
+})
+function callBack() {
+  console.log('第二个 回调');
+}
+ev.on('open', callBack);
+// removeEventListener
+ev.emit('open'); //触发
+ev.removeListener('open', callBack); //移除事件
+ev.emit('open');
+
+ev.once('one', (data) => {
+  console.log(data)
+})
+ev.emit('one', 'from emit data');
+ev.emit('one');
---
+++ node/event/lib/search.js
@@ -0,0 +1,9 @@
+const request = require('./request.js');
+module.exports = (keyWord) => {
+    keyWord = encodeURIComponent(keyWord);
+    const url='http://neteasecloudmusicapi.zhaoboy.com/search?keywords=' + keyWord;
+    // promise
+    return request(url);
+    // return keyWord;
+
+}
